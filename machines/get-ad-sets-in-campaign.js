c0e85723afad8ca1e3a2864a27530b971f0352ce
Colin Treseler <treseler@gmail.com>
2015-03-16 08:35:57 +0100

    putting mp in its own repo
    moving it out of a private repo

---
+++ machines/get-ad-sets-in-campaign.js
@@ -0,0 +1,77 @@
+module.exports = {
+
+
+  friendlyName: 'get ad sets in campaign',
+
+
+  description: 'get all ad sets in a campaign, and fetch standard metadata',
+
+
+  extendedDescription: 'get all ad sets for a given campaign group, and return id, daily budget, status',
+
+
+  inputs: {
+    adCampaignGroupId: {
+      example: '31231321312',
+      description: 'facebook adCampaignGroup is the endpoint for a campaign ',
+      required: true
+    },
+
+    accessToken: {
+      example: 'CAACEdEose0cBACBhZA7DJbYapwM7oZBt1EWhPiGqibBZAZAZCZCe6IOkfDRzrs1jyZCS93zSuj9GaNQQtxbny0jeSCqyBNaQUl3ocDiD3lO4GSboFm5B7NogSHFzTGYw0rdpndDKolQcfsS5nYeYwZAIKXF1WPzgGaGxNIDh36oZBHuazcN3WSNmL9jGyO9YmYlZBmZCcigBuMFvtXj4XlzNWyb',
+      description: 'this is the facebook issued access token for a given user and app pair',
+      required: true
+    },
+  },
+
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'The Facebook API returned an error (i.e. a non-2xx status code)',
+    },
+
+    success: {
+      description: 'Here are the ad sets for the inputted campaign',
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+    var doJSONRequest = require('../lib/do-request');
+
+    // GET ad accounts/ and send the api token as a header
+    doJSONRequest({
+      method: 'get',
+      url: ['/v2.2/', inputs.adCampaignGroupId ].join(""),
+      data: {
+        'access_token': inputs.accessToken,
+        'fields' : 'adcampaigns{id,daily_budget,campaign_status,stats}'
+      },
+      headers: {},
+    },
+    function (err, responseBody) {
+      if (err) { return exits.error(err); }
+
+      var myJson = responseBody.adcampaigns;
+      var newArray = [];
+      var len = myJson.data.length;
+      console.log(myJson.data.length);
+      for (var i=0; i<len; i++){
+        newArray.push({
+          'id' : myJson.data[i].id,
+          'daily_budget' : myJson.data[i].daily_budget,
+          'clicks' : myJson.data[i].stats.clicks,
+          'impressions' : myJson.data[i].stats.impressions
+         });
+       }
+       responseBody = newArray;
+      return exits.success(responseBody);
+    });
+  }
+};
---
+++ machines/get-ads-in-ad-set.js
@@ -0,0 +1,64 @@
+module.exports = {
+
+
+  friendlyName: 'get-ads-in-ad-set',
+
+
+  description: 'get ads in an ad set with standard metadata',
+
+
+  extendedDescription: 'get ads in an ad set and standard metadata',
+
+
+  inputs: {
+    adCampaignId: {
+      example: '31231321312',
+      description: 'an adcampaign is an adset',
+      required: true
+    },
+
+    accessToken: {
+      example: 'CAACEdEose0cBACBhZA7DJbYapwM7oZBt1EWhPiGqibBZAZAZCZCe6IOkfDRzrs1jyZCS93zSuj9GaNQQtxbny0jeSCqyBNaQUl3ocDiD3lO4GSboFm5B7NogSHFzTGYw0rdpndDKolQcfsS5nYeYwZAIKXF1WPzgGaGxNIDh36oZBHuazcN3WSNmL9jGyO9YmYlZBmZCcigBuMFvtXj4XlzNWyb',
+      description: 'this is the facebook issued access token for a given user and app pair',
+      required: true
+    },
+  },
+
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'The Facebook API returned an error (i.e. a non-2xx status code)',
+    },
+
+    success: {
+      description: 'Here are the ads in the ad set and their standard metadata',
+    },
+
+  },
+
+// get ad sets by campaign - first, find the right campaign id for a given name
+// second, fetch all the ad sets for that given campaign
+  fn: function (inputs,exits) {
+
+    var doJSONRequest = require('../lib/do-request');
+
+    // GET ad accounts/ and send the api token as a header
+    doJSONRequest({
+      method: 'get',
+      url: ['/v2.2/', inputs.adCampaignId, '/adgroups' ].join(""),
+      data: {
+        'access_token': inputs.accessToken,
+        'fields' : 'id,stats'
+      },
+      headers: {},
+    },
+    function (err, responseBody) {
+      if (err) { return exits.error(err); }
+      return exits.success(responseBody);
+    });
+  }
+};
