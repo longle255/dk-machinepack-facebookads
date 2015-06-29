002083b1b5b157a7fe6497bfcd11486dc2dab10e
Long Le <longle@Longs-USI-MacBook-Pro.local>
2015-06-29 12:09:52 +0200

    add query option for getting ad campagin

--- machines/get-ad-campaigns.js
+++ machines/get-ad-campaigns.js
@@ -45,15 +45,20 @@ module.exports = {
 
     var doJSONRequest = require('../lib/do-request');
     var fields = inputs.fields || 'name';
-
+    var data = {
+      'access_token': inputs.accessToken,
+      'fields': fields
+    };
+    if (inputs.queries) {
+      for (var i in inputs.queries) {
+        data[i] = inputs.queries[i];
+      }
+    }
     // GET ad accounts/ and send the api token as a header
     doJSONRequest({
       method: 'get',
       url: ['/v2.3/', inputs.adAccountId, '/adcampaign_groups'].join(""),
-      data: {
-        'access_token': inputs.accessToken,
-        'fields': fields
-      },
+      data: data,
       headers: {},
     }, function(err, responseBody) {
       if (err) {
